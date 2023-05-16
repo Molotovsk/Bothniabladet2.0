5581b1e2a0a9e1ed633e856efbc2207a89c6c007
Adam Jönsson <84878085+Molotovsk@users.noreply.github.com>
2023-05-16 14:04:17 +0200

    testat tailwind oc lagt tillbaka gamla registera

--- pages/account/register.jsx
+++ pages/account/register.jsx
@@ -38,5 +38,38 @@ function Register() {
             .catch(alertService.error);
     }
 
-    return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>;
+    return (
+            <div className="card">
+                <h4 className="card-header">Register</h4>
+                <div className="card-body">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="mb-3">
+                            <label className="form-label">First Name</label>
+                            <input name="firstName" type="text" {...register('firstName')} className={`form-control ${errors.firstName ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.firstName?.message}</div>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Last Name</label>
+                            <input name="lastName" type="text" {...register('lastName')} className={`form-control ${errors.lastName ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.lastName?.message}</div>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Username</label>
+                            <input name="username" type="text" {...register('username')} className={`form-control ${errors.username ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.username?.message}</div>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Password</label>
+                            <input name="password" type="password" {...register('password')} className={`form-control ${errors.password ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.password?.message}</div>
+                        </div>
+                        <button disabled={formState.isSubmitting} className="btn btn-primary">
+                            {formState.isSubmitting && <span className="spinner-border spinner-border-sm me-1"></span>}
+                            Register
+                        </button>
+                        <Link href="/account/login" className="btn btn-link">Cancel</Link>
+                    </form>
+                </div>
+            </div>
+    );
 }
