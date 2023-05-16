3b67d544e093f19030b1e8d43c41f369e87f8385
Adam Jönsson <84878085+Molotovsk@users.noreply.github.com>
2023-05-16 11:40:53 +0200

    a

--- pages/account/login.jsx
+++ pages/account/login.jsx
@@ -5,6 +5,8 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import Image from "next/image";
 
+import loginImg from 'pages/assets/Bothniabladet.png'
+
 import { Layout } from 'components/account';
 import { userService, alertService } from 'services';
 
@@ -36,7 +38,35 @@ function Login() {
     }
 
     return (
-        <Layout>
+
+            <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+                <div className='hidden sm:block'>
+                    <img className='w-full h-full object-cover' src={loginImg} alt="" />
+                </div>
+        
+                <div className='bg-gray-100 flex flex-col justify-center'>
+                    <form className='max-w-[400px] w-full mx-auto bg-white p-4'>
+                        <h2 className='text-4xl font-bold text-center py-6'>Bothniabladet.</h2>
+                        <div className='flex flex-col py-2'>
+                            <label>Användarnamn</label>
+                            <input className='border p-2' type="text" />
+                        </div>
+                        <div className='flex flex-col py-2'>
+                            <label>Password</label>
+                            <input className='border p-2' type="password" />
+                        </div>
+                        <button className='border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white'>Sign In</button>
+                        <div className='flex justify-between'>
+                            <p className='flex items-center'><input className='mr-2' type="checkbox" /> Remember Me</p>
+                            <p>Create an account</p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+    );  
+
+
+        /*<Layout>
 
             <header className="flex justify-center">
 
@@ -45,6 +75,7 @@ function Login() {
             <Link href="/startPage">
                         <Image
                                 class="basis-1/8 shrink"
+                                import loginImg from 'pages/assets/Bothniabladet.png'
                                 src={require('pages/assets/Bothniabladet.png')}
                                 alt="Bothniabladet logga"
                                 width={1000}
@@ -80,6 +111,6 @@ function Login() {
                 </div>
             </div>
             
-        </Layout>
-    );
+        </Layout> */
+    
 }
