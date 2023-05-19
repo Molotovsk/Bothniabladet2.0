024fd6e6220d4d411e366a1b4fc8932b51fa44dd
ellafloding <ella@floding.se>
2023-05-19 21:08:11 +0200

    Går att söka på tags och titlar och få upp array med resultat i console log

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import { SearchBar } from 'components/SearchBar';
+import {getQuery, SearchBar} from 'components/SearchBar';
 import { search, mapImageResources } from '../helpers/cloudinary';
+import {SelectedImages} from "@/pages/SelectedImages";
 
+// let images = [];
+let allImages = [];
 export default function Home({ images: defaultImages, totalCount: defaultTotalCount }) {
   const [images, setImages] = useState(defaultImages);
   const [totalCount, setTotalCount] = useState(defaultTotalCount);
@@ -22,32 +25,31 @@ export default function Home({ images: defaultImages, totalCount: defaultTotalCo
   };
 
 
-
-
-    // Function to handle opening the modal
-    const handleOpenModal = (image) => {
-      setSelectedImage(image);
-      setModalOpen(true);
-    };
-  
-    // Function to handle closing the modal
-    const handleCloseModal = () => {
-      setSelectedImage(null);
-      setModalOpen(false);
-    }
+    // // Function to handle opening the modal
+    // const handleOpenModal = (image) => {
+    //   setSelectedImage(image);
+    //   setModalOpen(true);
+    // };
+    //
+    // // Function to handle closing the modal
+    // const handleCloseModal = () => {
+    //   setSelectedImage(null);
+    //   setModalOpen(false);
+    // }
 
 
 
     return (
       <div className="min-h-screen mx-auto px-1 bg-myColor-100  flex-col justify-center items-center">
 
-        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} handleSearch={handleSearch} />
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} handleSearch={searchFunction} />
 
   
       <h2 className="text-2xl font-bold mt-12 mb-4">Bilder</h2>
   
       <ul className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-6 gap-x-6 gap-y-8">
         {images.map((image) => {
+          allImages = images;
           return (
             <li key={image.id} className="flex flex-col items-center">
               <div className="aspect-w-2 aspect-h-4">
@@ -66,7 +68,9 @@ export default function Home({ images: defaultImages, totalCount: defaultTotalCo
   
 }
 
-
+export function getAllImages(){
+  return allImages;
+}
 
 export async function getStaticProps() {
   const results = await search({ expression: '' });
@@ -79,4 +83,29 @@ export async function getStaticProps() {
       totalCount: images.length,
     },
   };
+}
+function compareStrings(s1, s2){
+  if(s1 == s2){
+    return true;
+  }
+  return false;
+}
+
+export function searchFunction(){
+  const query = getQuery();
+  let i = 0;
+  let selectedImages = [];
+  console.log(allImages);
+
+  var result = allImages.filter(obj => {
+    if(compareStrings(query.toUpperCase(), obj.title.toUpperCase()))
+    return obj;
+    else if(obj.tags.includes(query)){
+      return obj;
+    }
+  })
+  selectedImages = result;
+  console.log(selectedImages);
+  return selectedImages;
+
 }
