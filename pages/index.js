0d9e63d5c911a45d12ee929d451789e22990b390
ellafloding <ella@floding.se>
2023-05-20 23:08:16 +0200

    Fixade sökningen på bilder

--- pages/index.js
+++ pages/index.js
@@ -54,24 +54,40 @@ export default function Home({images: defaultImages, totalCount: defaultTotalCou
     console.log(query)
   }
   function getQuery(){
+      if(query){
+          return query.trim();
+      }
     return query;
   }
+  //Clears the searchbox
+    function clearInput(){
+        var getValue= document.getElementById("searchBox");
+        if (getValue) {
+            getValue.value = "";
+        }
+    }
 function searchFunction() {
     const query = getQuery();
     let i = 0;
     let selectedImages = [];
     console.log(allImages);
 
-    var result = allImages.filter(obj => {
-      if (compareStrings(query.toUpperCase(), obj.title.toUpperCase())) {
-        return obj;
-      } else if (obj.tags.includes(query)) {
-        return obj;
-      }
-    })
+    if(query) {
+        var result = allImages.filter(obj => {
+            if (compareStrings(query.toUpperCase(), obj.title.toUpperCase())) {
+                return obj;
+            } else if (obj.tags.includes(query)) {
+                return obj;
+            }
+        })
+    }
     selectedImages = result;
+    if(!selectedImages){
+        selectedImages = [];
+    }
     console.log(selectedImages);
     setSearchedImages(selectedImages);}
+    clearInput();
 
     return (
       <div className="min-h-screen mx-auto px-1 bg-myColor-100  flex-col justify-center items-center" id="map">
@@ -79,6 +95,7 @@ function searchFunction() {
         <div className="flex justify-center items-center">
         <input type="text" der="Sök efter bilder här..."
                className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+               id="searchBox"
                value={query}
                onChange={(e) => setQuery(e.target.value)} />
 
@@ -93,7 +110,6 @@ function searchFunction() {
         {/*<SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} handleSearch={SelectedImages} />*/}
 
         <h2 className="text-2xl font-bold mt-12 mb-4">Sökta bilder</h2>
-
         {searchedImages.map((image) => {
           return (
               <li key={image.id} className="flex flex-col items-center">
@@ -106,7 +122,7 @@ function searchFunction() {
               </li>
           );
         })}
-  
+
       <h2 className="text-2xl font-bold mt-12 mb-4">Alla bilder</h2>
 
       <ul className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-6 gap-x-6 gap-y-8">
