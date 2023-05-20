b1516c79d437cdd8a56f6909527842f6a67a310d
Adam Jönsson <84878085+Molotovsk@users.noreply.github.com>
2023-05-20 19:03:18 +0200

    TEstar testar

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import { mapImageResources } from '../helpers/cloudinary';
-import { performSearch } from './api/search';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { useRouter } from 'next/router'
-import { useLastViewedPhoto } from '../utils/useLastViewedPhoto'
-import { useEffect, useRef } from 'react'
-import { ImageProps } from '../utils/types'
-import Modal from '../components/Modal'
+import { performSearch } from './api/search';
+import { useLastViewedPhoto } from '../utils/useLastViewedPhoto';
+import { mapImageResources } from '../helpers/cloudinary';
+
 
 
 function SearchBar({ handleSearch }) {
@@ -82,29 +80,29 @@ export default function Home({ images: defaultImages, totalCount: defaultTotalCo
       <h2 className="text-2xl font-bold mt-12 mb-4">Bilder</h2>
   
       <ul className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-6 gap-x-6 gap-y-8">
-        {images.map(({ id, public_id, format }) => (
-              <Link
-                key={id}
-                href={`/?photoId=${id}`}
-                as={`/p/${id}`}
-                ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef.current : null}                
-                shallow
-                
-                >
-                  <Image
-                    alt={'Bildtext saknas..'}
-                    style={{ transform: 'translate3d(0, 0, 0)' }}
-                    src={`https://res.cloudinary.com/dmhozrlru/image/upload/c_scale,w_720/${public_id}.${format}`}
-                    width={720}
-                    height={480}
-                    sizes="(max-width: 640px) 100vw,
-                    (max-width: 1280px) 50vw,
-                    (max-width: 1536px) 33vw,
-                    25vw"
-                    />
-                </Link>
-        ))}
-      </ul>
+  {images.map(({ id, public_id, format }) => (
+    <Link
+      key={id}
+      href={`/?photoId=${id}`}
+      as={`/p/${id}`}
+      ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
+      shallow
+    >
+      <Image
+        alt={'Bildtext saknas..'}
+        style={{ transform: 'translate3d(0, 0, 0)' }}
+        src={`https://res.cloudinary.com/dmhozrlru/image/upload/c_scale,w_720/${public_id}.${format}`}
+        width={720}
+        height={480}
+        sizes="(max-width: 640px) 100vw,
+        (max-width: 1280px) 50vw,
+        (max-width: 1536px) 33vw,
+        25vw"
+      />
+      <h1>{images.id}</h1>
+    </Link>
+  ))}
+</ul>
     </div>
   );
   
@@ -112,21 +110,22 @@ export default function Home({ images: defaultImages, totalCount: defaultTotalCo
 
 export async function getStaticProps() {
   const results = await performSearch({ expression: '' }); // Modify the default expression value here if needed
-  const { resources } = results;
+
 
   let reducedResults = [];
   let i = 0;
-  for (let result of resources) {
+  for (let result of results.resources) {
     reducedResults.push({
-      id: i++, // Increment `i` before assigning it to `id`
+      id: i, // Increment `i` before assigning it to `id`
       height: result.height,
       width: result.width,
       public_id: result.public_id,
       format: result.format,
-    });
+    })
+    i++
   }
 
-  const images = mapImageResources(reducedResults);
+  //const images = mapImageResources(reducedResults);
 
   return {
     props: {
@@ -141,96 +140,3 @@ export async function getStaticProps() {
 
 
 
-/*
-
-function SearchBar({ handleSearch }) {
-  const [query, setQuery] = useState('');
-
-  const handleInputChange = (e) => {
-    const value = e.target.value;
-    setQuery(value);
-    console.log('Search input:', value); // Log the input value
-  };
-
-  const handleButtonClick = () => {
-    handleSearch(query);
-  };
-
-  return (
-    <div className="flex justify-center items-center">
-      <input
-        type="text"
-        placeholder="Sök efter bilder här..."
-        className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-        value={query}
-        onChange={handleInputChange}
-      />
-      <button
-        className="px-4 py-2 ml-2 text-white bg-myColor-700 rounded-full hover:bg-myColor-300 focus:outline-none"
-        onClick={handleButtonClick}
-      >
-        Sök
-      </button>
-    </div>
-  );
-}
-
-
-export default function Home({ images: defaultImages, totalCount: defaultTotalCount }) {
-  const [images, setImages] = useState(defaultImages);
-  const [totalCount, setTotalCount] = useState(defaultTotalCount);
-
-  const handleSearch = async (query) => {
-    try {
-      console.log('Searching for:', query);
-  
-      const results = await performSearch({ expression: query });
-      const { resources } = results;
-      const updatedImages = mapImageResources(resources);
-  
-      setImages(updatedImages);
-      setTotalCount(updatedImages.length);
-    } catch (error) {
-      console.error('Error occurred during search:', error);
-    }
-  };
-
-
-  return (
-    <div className="min-h-screen mx-auto px-1 bg-myColor-100  flex-col justify-center items-center">
-      <SearchBar handleSearch={handleSearch} />
-
-      <h2 className="text-2xl font-bold mt-12 mb-4">Bilder</h2>
-
-      <ul className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-6 gap-x-6 gap-y-8">
-        {images.map((image) => (
-          <li key={image.id} className="flex flex-col items-center">
-            <div className="aspect-w-2 aspect-h-4">
-              <Link href={`/pictures/${image.id}`}>
-                <Image width={image.width} height={image.height} src={image.image} alt="hej" className="object-cover" />
-              </Link>
-            </div>
-            <a href={image.title} className="container mx-auto px-1 mt-2 text-current font-semibold">
-              <h1>{image.title}</h1>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export async function getStaticProps() {
-  const results = await performSearch({ expression: '' }); // Modify the default expression value here if needed
-  const { resources } = results;
-  const images = mapImageResources(resources);
-
-  return {
-    props: {
-      images,
-      totalCount: images.length,
-    },
-  };
-}
-
-*/
--- pages/pictures/[photoId].js
+++ pages/pictures/[photoId].js
@@ -1,15 +1,20 @@
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import { getResults } from '../utils/cachedImages';
-import ImageDetails from '../components/ImageDetails';
-
+import { useState } from 'react';
 
 const Home = ({ currentPhoto }) => {
   const router = useRouter();
-  const { photoId } = router.query
+  const { photoId } = router.query;
   let index = Number(photoId);
 
   const currentPhotoUrl = `https://res.cloudinary.com/dmhozrlru/image/upload/c_scale,w_2560/${currentPhoto.public_id}.${currentPhoto.format}`;
+
+  const [isAddedToCart, setIsAddedToCart] = useState(false);
+
+  const handleAddToCart = () => {
+    // Placeholder logic to add the item to the cart
+    setIsAddedToCart(true);
+  };
 
   return (
     <>
@@ -18,36 +23,35 @@ const Home = ({ currentPhoto }) => {
         <meta property="og:image" content={currentPhotoUrl} />
       </Head>
       <main className="mx-auto max-w-[1960px] p-4">
-        <ImageDetails photo={currentPhoto} />
+        <h1>Photo ID: {photoId}</h1>
+        <img src={currentPhotoUrl} alt="Current Photo" />
+
+        {/* Display other photo information here */}
+        <p>Created at: {currentPhoto.created_at}</p>
+        <p>Dimensions: {currentPhoto.width} x {currentPhoto.height}</p>
+
+        {!isAddedToCart ? (
+          <button onClick={handleAddToCart}>Add to Cart</button>
+        ) : (
+          <p>Item added to cart!</p>
+        )}
       </main>
     </>
   );
 };
 
-export default Home;
-
 export async function getStaticProps(context) {
-  const results = await getResults();
+  // Fetch the photo data based on the id parameter
+  const { photoId } = context.params;
 
-  let reducedResults = [];
-  let i = 0;
-  for (let result of results.resources) {
-    reducedResults.push({
-      id: i,
-      height: result.height,
-      width: result.width,
-      public_id: result.public_id,
-      format: result.format,
-      imageUrl: `https://res.cloudinary.com/dmhozrlru/image/upload/c_scale,w_720/${result.public_id}.${result.format}`, // Add imageUrl property
-      title: 'Image Title', // Replace with the actual title property of your image
-      description: 'Image Description', // Replace with the actual description property of your image
-    });
-    i++;
-  }
-
-  const currentPhoto = reducedResults.find(
-    (img) => img.id === Number(context.params.photoId)
-  );
+  // Replace this with your actual logic to fetch the photo data
+  const currentPhoto = {
+    public_id: 'sample_public_id',
+    created_at: '2023-05-20',
+    width: 1024,
+    height: 768,
+    format: 'jpg',
+  };
 
   return {
     props: {
@@ -57,127 +61,18 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
-  const results = await performSearch({ expression: '' }); // Modify the default expression value here if needed
+  // Fetch the available photo IDs or generate them dynamically
+  const photoIds = [1, 2, 3]; // Replace with your actual logic to fetch photo IDs
 
-  let fullPaths = []
-  for (let i = 0; i < results.resources.length; i++) {
-    fullPaths.push({ params: { photoId: i.toString() } })
-  }
+  // Generate an array of path objects with params containing the photo IDs
+  const paths = photoIds.map((id) => ({
+    params: { photoId: id.toString() },
+  }));
 
   return {
-    paths: fullPaths,
+    paths: paths,
     fallback: false,
-  }
+  };
 }
 
-
-
-
-/*
-
-
-
-import type { GetStaticProps, NextPage } from 'next'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
-import Carousel from '../../components/Carousel'
-import getResults from '../../utils/cachedImages'
-import cloudinary from '../../utils/cloudinary'
-import getBase64ImageUrl from '../../utils/generateBlurPlaceholder'
-import type { ImageProps } from '../../utils/types'
-
-const Home: NextPage = ({ currentPhoto }: { currentPhoto: ImageProps }) => {
-  const router = useRouter()
-  const { photoId } = router.query
-  let index = Number(photoId)
-
-  const currentPhotoUrl = `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_2560/${currentPhoto.public_id}.${currentPhoto.format}`
-
-  return (
-    <>
-      <Head>
-        <title>Next.js Conf 2022 Photos</title>
-        <meta property="og:image" content={currentPhotoUrl} />
-        <meta name="twitter:image" content={currentPhotoUrl} />
-      </Head>
-      <main className="mx-auto max-w-[1960px] p-4">
-        <Carousel currentPhoto={currentPhoto} index={index} />
-      </main>
-    </>
-  )
-}
-
-export default Home
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const results = await getResults()
-
-  let reducedResults: ImageProps[] = []
-  let i = 0
-  for (let result of results.resources) {
-    reducedResults.push({
-      id: i,
-      height: result.height,
-      width: result.width,
-      public_id: result.public_id,
-      format: result.format,
-    })
-    i++
-  }
-
-  const currentPhoto = reducedResults.find(
-    (img) => img.id === Number(context.params.photoId)
-  )
-  currentPhoto.blurDataUrl = await getBase64ImageUrl(currentPhoto)
-
-  return {
-    props: {
-      currentPhoto: currentPhoto,
-    },
-  }
-}
-
-export async function getStaticPaths() {
-  const results = await cloudinary.v2.search
-    .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
-    .sort_by('public_id', 'desc')
-    .max_results(400)
-    .execute()
-
-  let fullPaths = []
-  for (let i = 0; i < results.resources.length; i++) {
-    fullPaths.push({ params: { photoId: i.toString() } })
-  }
-
-  return {
-    paths: fullPaths,
-    fallback: false,
-  }
-}
-
-
-export default Picture;
-
-function Picture() {
-    const router = useRouter();
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        const { id } = router.query;
-        if (!id) return;
-
-        // fetch user and set default form values if in edit mode
-        userService.getById(id)
-            .then(x => setUser(x))
-            .catch(alertService.error)
-    }, [router]);
-
-    return (
-        <Layout>
-            <h1>Edit User</h1>
-            {user ? <AddEdit user={user} /> : <Spinner />}
-        </Layout>
-    );
-}
-
-*/
+export default Home;
